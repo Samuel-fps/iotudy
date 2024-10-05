9255eec02a6657bc530746f5e4eb3a9b78457374
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-10-05 20:14:48 +0200

    fix-navbar-togle

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,4 @@
-import 'bootstrap';
+// resources/js/app.js
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importar Bootstrap
+import 'bootstrap'; // Importar JavaScript de Bootstrap
 import '../css/app.css';
