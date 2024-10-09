a0de19bee9a5e960332ec6f3837c90f318fb4ae6
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-10-09 12:29:52 +0200

    fix-nette-dependency

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -345,8 +345,8 @@ class ComposerStaticInitf500f00881b4b8fcf629b39203f8c10c
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
